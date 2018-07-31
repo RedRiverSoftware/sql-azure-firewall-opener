60ad95b094d3eac4ba1eb222459e84b5de7aec1c
Kieren Johnstone <kierenj@gmail.com>
2018-07-31 09:07:33 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM microsoft/azure-cli:2.0.42
+
+ADD azsqlfirewall.sh /azsqlfirewall.sh
+RUN chmod +x /azsqlfirewall.sh
+CMD ["/bin/bash", "/azsqlfirewall.sh"]
---
+++ azsqlfirewall.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+defaultrulename=azsql_$(hostname -s)
+rulename=${rulename:-$defaultrulename}
+
+bad=0
+if [ -z "$username" ]; then echo "variable 'username' is not set"; bad=1; fi
+if [ -z "$password" ]; then echo "variable 'password' is not set"; bad=1; fi
+if [ -z "$fqdn" ]; then echo "variable 'fqdn' is not set"; bad=1; fi
+if [ $bad -eq 1 ]
+then
+	echo "please set variables: username, password, fqdn, rulename"
+	echo "note: if rulename is not set, azsql_HOSTNAME will be used"
+	exit 1
+fi
+
+echo getting external IP...
+externalip=$(curl -s http://whatismyip.akamai.com/)
+echo external IP is $externalip
+
+echo logging on to Azure...
+az login -u $username -p $password &> /dev/null
+retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo login failed
+	exit 1
+fi
+
+echo logged on. getting server list...
+servers=$(az sql server list)
+retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo error getting server list
+	exit 1
+fi
+
+id=$(echo "$servers" | jq -r --arg fqdn $fqdn '.[] | select(.fullyQualifiedDomainName | contains($fqdn)) | .id')
+resgrp=$(echo "$servers" | jq -r --arg fqdn $fqdn '.[] | select(.fullyQualifiedDomainName | contains($fqdn)) | .resourceGroup')
+srvname=$(echo "$servers" | jq -r --arg fqdn $fqdn '.[] | select(.fullyQualifiedDomainName | contains($fqdn)) | .name')
+
+if [ -z "$id" ]; then echo "server not found in list"; exit 1; fi
+
+echo "server found, getting existing rules..."
+rules=$(az sql server firewall-rule list --id=$id)
+retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo error getting firewall rules
+	exit 1
+fi
+
+existing=$(echo "$rules" | jq -r --arg externalip $externalip '.[] | select(.startIpAddress | contains($externalip)) | .id')
+
+if [ -n "$existing" ]; then echo "existing rule already covers IP address"; exit 0; fi
+
+namecheck=$(echo "$rules" | jq -r --arg rulename $rulename '.[] | select(.name | contains($rulename)) | .name')
+
+if [ -n "$namecheck" ]
+then
+	echo "updating existing rule with specified name"
+	az sql server firewall-rule update -g=$resgrp -s=$srvname --name=$rulename --start-ip-address=$externalip --end-ip-address=$externalip
+else
+	echo "creating new rule (no existing rule found with specified name)"
+	az sql server firewall-rule create -g=$resgrp -s=$srvname --name=$rulename --start-ip-address=$externalip --end-ip-address=$externalip
+fi
